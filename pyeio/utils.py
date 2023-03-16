6ec8d9ee576f41907fcdc9c8b49c168e05129732
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2023-03-16 14:18:10 -0400

    pyeio/utils: move Query class into utils

--- pyeio/utils.py
+++ pyeio/utils.py
@@ -1,27 +1,26 @@
 from pathlib import Path
+from typing import Any
 
 
-class Query:
-    @staticmethod
-    def file_name(path: str | Path) -> str:
-        "get file name from path"
-        return str(path).split("/")[-1]
+def file_name(path: str | Path) -> str:
+    "get file name from path"
+    return str(path).split("/")[-1]
 
-    @staticmethod
-    def file_format(path: str | Path) -> str:
-        "get file format from path"
-        return Query.file_name(path).split(".")[-1]
 
-    @staticmethod
-    def file_size(path: str | Path) -> int:
-        "get file size in bytes from path"
-        raise NotImplementedError()
+def file_format(path: str | Path) -> str:
+    "get file format from path"
+    return file_name(path).split(".")[-1]
 
-    @staticmethod
-    def data_type(obj: Any) -> str:
-        "get object type as string"
-        return str(type(obj)).split("'")[-2]
 
-    @staticmethod
-    def read():
-        pass
+def file_size(path: str | Path) -> int:
+    "get file size in bytes from path"
+    raise NotImplementedError()
+
+
+def data_type(obj: Any) -> str:
+    "get object type as string"
+    return str(type(obj)).split("'")[-2]
+
+
+def read():
+    pass
