debf41bf9db46e3ecd9b6f54aa713dd0dcf177c6
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2023-03-16 14:20:49 -0400

    pyeio/builder: simplify builder

--- pyeio/builder.py
+++ pyeio/builder.py
@@ -1,20 +1,6 @@
 class IO:
     def __init__(self):
-        self.__init_utility_subclasses()
-        self.__init_interface_subclasses()
         self.__init_interface_dict()
-
-    def __init_utility_subclasses(self) -> None:
-        self.query = Query()
-        self.transform = Transform()
-
-    def __init_interface_subclasses(self) -> None:
-        self.json = JSON()
-        self.jsonl = JSONL()
-        self.csv = CSV()
-        self.xlsx = XLSX()
-        self.yaml = YAML()
-        self.toml = TOML()
 
     def __init_interface_dict(self) -> None:
         self._id = {
