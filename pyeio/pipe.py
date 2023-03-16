a63136d1a49534451180e9f9da0ba2cbb23508c1
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2023-03-16 15:04:30 -0400

    pyeio/pipe: create

---
+++ pyeio/pipe.py
@@ -0,0 +1,9 @@
+"""
+TODO:
+- implement pipeline class that allows loading and format transformations with load
+    - useful when you want to load a zip file with internal formats, for instance
+"""
+
+
+class Pipeline:
+    pass
