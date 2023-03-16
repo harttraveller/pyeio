bba16bb822818614cfa29d0795d0b798937427fa
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2023-03-15 22:12:53 -0400

    pyeio/builder: add jsonl to interface dict

--- pyeio/builder.py
+++ pyeio/builder.py
@@ -198,7 +198,10 @@ class IO:
         self.toml = TOML()
 
     def __init_interface_dict(self) -> None:
-        self._id = {"json": {"save": self.json.save, "load": self.json.load}}
+        self._id = {
+            "json": {"save": self.json.save, "load": self.json.load},
+            "jsonl": {"save": self.jsonl.save, "load": self.jsonl.load},
+        }
 
     @property
     def formats(self) -> set[str]:
