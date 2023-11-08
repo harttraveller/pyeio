908d5c082aadf6e1138153fad9e0e3eb078ec84f
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2023-11-07 19:22:50 -0500

    pyeio/core: update

--- pyeio/core.py
+++ pyeio/core.py
@@ -10,7 +10,7 @@ from pathlib import Path
 from typing import Any, Union, Optional
 from pydantic import BaseModel
 from pyeio.util import file_format
-from pyeio.schema import JSON, JSONL
+from pyeio.schema import FORMATS
 
 
 class DataFormat(BaseModel):
@@ -40,52 +40,45 @@ class ResolutionResult(BaseModel):
 #         pass
 
 
-def resolve_via_loc(loc: Union[str, Path], val: bool) -> ResolutionResult:
-    loc = str(loc)
-    if "." in loc:
-        extension = loc.split(".")[-1].lower()
-        # if extension in extset:
-        #     return ResolutionResult(success=True, dformat=DataFormat(extension=extension))
-        # else:
-        #     return ResolutionResult(
-        #         success=False, dformat=DataFormat(extension="unknown")
-        #     )
-    else:
-        # todo: if val, load
-        # todo: if url/uri, doublecheck by retrieving from web
-        # todo: if path, doublecheck by loading in
-        raise NotImplementedError(
-            "Could not resolve from file extension, would need to read in file "
-            "to resolve from binary, but this feature is currently unimplemented."
-        )
+# def resolve_via_loc(loc: Union[str, Path], val: bool) -> ResolutionResult:
 
 
-def resolve_via_raw(raw: Union[str, bytes]) -> ResolutionResult:
-    pass
+# def resolve_via_raw(raw: Union[str, bytes]) -> ResolutionResult:
+#     pass
 
 
 class Easy:
-    def __resolve_format_via_raw():
-        pass
+    def __resolve_format_via_loc(self, loc: Union[str, Path]) -> DataFormat:
+        loc = str(loc)
+        if "." in loc:
+            extension = loc.split(".")[-1].lower()
+            # if extension in extset:
+            #     return ResolutionResult(success=True, dformat=DataFormat(extension=extension))
+            # else:
+            #     return ResolutionResult(
+            #         success=False, dformat=DataFormat(extension="unknown")
+            #     )
+        else:
+            # todo: if val, load
+            # todo: if url/uri, doublecheck by retrieving from web
+            # todo: if path, doublecheck by loading in
+            raise NotImplementedError(
+                "Could not resolve from file extension, would need to read in file "
+                "to resolve from binary, but this feature is currently unimplemented."
+            )
 
-    def __resolve_format_via_loc():
+    def __resolve_format_via_raw(self, raw: Union[str, bytes]) -> DataFormat:
         pass
 
     def identify(
         self,
         raw: Optional[Union[str, bytes]] = None,
         loc: Optional[Union[str, Path]] = None,
-        val: bool = False,
     ) -> DataFormat:
         """
         raw: text
         loc: path (str/Path) or uri/url to resource
         """
-        if val:
-            raise NotImplementedError(
-                "Reading in the resource to check it is not yet supported, "
-                "you should read it in and pass the text or binary data to this function."
-            )
         # check if appropriate params have been passed in
         if all([raw is None, loc is None]):
             raise ValueError(
@@ -94,12 +87,12 @@ class Easy:
         # assess if the path or uri
         # todo: if val, run all, compare results
         if loc is not None:
-            loc_resolution_attempt = resolve_via_loc(loc=loc, val=val)
+            loc_resolution_attempt = self.__resolve_format_via_loc(loc=loc)
             if loc_resolution_attempt.success:
                 # todo: if val, doublecheck by reading in
                 return loc_resolution_attempt.dformat
         if raw is not None:
-            raw_resolution_attempt = resolve_via_raw(raw=raw)
+            raw_resolution_attempt = self.__resolve_format_via_raw(raw=raw)
             if raw_resolution_attempt.success:
                 return raw_resolution_attempt.dformat
         # todo: check to make sure there are no dataformats called 'unknown'
