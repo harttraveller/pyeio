564f9d558e913a0cc8214dab4a712f726001dcba
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2024-08-25 12:39:31 -0700

    pyeio/core/types: update

--- pyeio/core/types.py
+++ pyeio/core/types.py
@@ -6,26 +6,33 @@ PathLike = str | Path
 FileExtension = Literal[
     "json",
     "jsonl",
+    "toml",
+    "yaml",
+    "xml",
+    "md",
+    "jpeg",
     "zst",
 ]
 
 file_extensions: tuple[str, ...] = get_args(FileExtension)
 
-StandardFileExtension = Literal[
-    "json",
-    "jsonl",
-    "toml",
-    "xml",
-    "md",
-    "jpeg",
-]
-
-standard_file_extensions: tuple[str, ...] = get_args(StandardFileExtension)
 
 VariantFileExtension = Literal[
     "ndjson",
     "jsonlines",
+    "jpg",
+    "yml",
+    "markdown",
 ]
 
 
 variant_file_extensions: tuple[str, ...] = get_args(VariantFileExtension)
+
+
+variant_to_standard: dict[VariantFileExtension, FileExtension] = {
+    "ndjson": "jsonl",
+    "jsonlines": "jsonl",
+    "jpg": "jpeg",
+    "yml": "yaml",
+    "markdown": "md",
+}
