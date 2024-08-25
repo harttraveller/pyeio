406f723d823c19844d54a7deb7e3eaeb6a6b4043
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2024-08-25 12:25:57 -0700

    pyeio/core/types: update

--- pyeio/core/types.py
+++ pyeio/core/types.py
@@ -1,3 +1,11 @@
 from pathlib import Path
+from typing import Literal, get_args
 
 PathLike = str | Path
+
+FileExtension = Literal[
+    "json",
+    "jsonl",
+]
+
+file_extensions = get_args(FileExtension)
