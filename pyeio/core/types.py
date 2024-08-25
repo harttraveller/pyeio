95494277c5a9ca229bf779bfd0fedbca8a72b588
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2024-08-25 13:48:56 -0700

    pyeio/core/types: comment stuff

--- pyeio/core/types.py
+++ pyeio/core/types.py
@@ -5,41 +5,42 @@ PathLike = str | Path
 
 FileExtension = Literal[
     "json",
-    "jsonl",
-    "toml",
-    "yaml",
-    "xml",
-    "md",
-    "jpeg",
-    "zst",
-    "nc",
+    # "jsonl",
+    # "toml",
+    # "yaml",
+    # "xml",
+    # "md",
+    # "jpeg",
+    # "zst",
+    # "nc",
 ]
 
 file_extensions: tuple[str, ...] = get_args(FileExtension)
 
 
-VariantFileExtension = Literal[
-    "ndjson",
-    "jsonlines",
-    "jpg",
-    "yml",
-    "markdown",
-]
+# VariantFileExtension = Literal[
+#     "ndjson",
+#     "jsonlines",
+#     "jpg",
+#     "yml",
+#     "markdown",
+# ]
 
 
-variant_file_extensions: tuple[str, ...] = get_args(VariantFileExtension)
+# variant_file_extensions: tuple[str, ...] = get_args(VariantFileExtension)
 
 MimeType = Literal[
-    "application/jsonl",
-    "application/netcdf",
-    "application/x-netcdf",
+    "application/json",
+    # "application/jsonl",
+    # "application/netcdf",
+    # "application/x-netcdf",
 ]
 
 
-variant_to_standard: dict[VariantFileExtension, FileExtension] = {
-    "ndjson": "jsonl",
-    "jsonlines": "jsonl",
-    "jpg": "jpeg",
-    "yml": "yaml",
-    "markdown": "md",
-}
+# variant_to_standard: dict[VariantFileExtension, FileExtension] = {
+#     "ndjson": "jsonl",
+#     "jsonlines": "jsonl",
+#     "jpg": "jpeg",
+#     "yml": "yaml",
+#     "markdown": "md",
+# }
