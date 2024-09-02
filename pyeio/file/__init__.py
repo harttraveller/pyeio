3ac9dd019901dca7aed6d0c26e29afbd55e1a70c
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2024-09-01 21:16:21 -0700

    move stuff

--- pyeio/file/__init__.py
+++ pyeio/file/__init__.py
@@ -1,30 +1,3 @@
 from . import (
     json,
 )
-
-
-def open(): ...
-
-
-def read(): ...
-
-
-def save(): ...
-
-
-def prepend(): ...
-
-
-def append(): ...
-
-
-def decompress(): ...
-
-
-def compress(): ...
-
-
-def apply(): ...
-
-
-def traverse(): ...
