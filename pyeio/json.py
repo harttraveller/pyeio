0dc96c2d9dddc49f2cbbd4e38833e912ce0edbe8
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2024-09-02 14:04:32 -0700

    pyeio/json: update

--- pyeio/json.py
+++ pyeio/json.py
@@ -1,36 +1,32 @@
+import json as pyjson
 from pathlib import Path
-from typing import TypeVar, Generator
-from pyeio.core.exceptions import MissingExtraError
-from pyeio.core import io
-
-try:
-    import orjson
-except ImportError:
-    raise MissingExtraError(extra="json")
+from typing import TypeVar, Generator, Optional
+from pyeio import text
 
 
 T = TypeVar("T", bound="JSON")
 JSON = bool | int | float | str | list[T] | dict[str, T]
 
 
-def open(path: str | Path) -> JSON:
-    return orjson.loads(io.load_text(path=Path(path)))
+def parse(data: str | bytes) -> JSON:
+    return pyjson.loads(data)
 
 
-def walk(path: str | Path) -> Generator[tuple[str, JSON], None, None]:
-    for file in Path(path).glob("**/*.json"):
-        yield (str(file.absolute()), open(file))
+def dump(data: JSON) -> str:
+    return pyjson.dumps(data)
+
+
+def load(path: str | Path) -> JSON:
+    return parse(text.load(path=Path(path)))
 
 
 def save():
     raise NotImplementedError()
 
 
-def load(data: str | bytes) -> JSON: ...
-
-
-def dump(data: JSON, indent: int | None = None) -> str:
-    raise NotImplementedError()
+def walk(path: str | Path) -> Generator[tuple[str, JSON], None, None]:
+    for file in Path(path).glob("**/*.json"):
+        yield (str(file.absolute()), load(file))
 
 
 # from pathlib import Path
