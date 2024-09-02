bf0a60073a9a4257e6f7d1a409e886d23dff2ca1
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2024-09-02 11:10:44 -0700

    pyeio: update

--- pyeio/json.py
+++ pyeio/json.py
@@ -41,3 +41,22 @@ def load(data: str | bytes) -> JSON: ...
 
 def dump(data: JSON, indent: int | None = None) -> str:
     raise NotImplementedError()
+
+
+# from pathlib import Path
+
+
+# def crawl(): ...
+
+
+# def get(url: str):
+#     raise NotImplementedError()
+
+
+# def download(url: str, path: str | Path):
+#     raise NotImplementedError()
+
+
+# # # todo: get recursive from webpage or online directory
+# # # webpage: eg - scrape all json links and download to local dir
+# # # dir: eg - s3 bucket, dl all
