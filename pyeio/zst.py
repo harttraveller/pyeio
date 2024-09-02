ac7dedfacb3e8f341f83dbcaa4a86b3b789d7bed
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2024-09-02 00:38:50 -0700

    pyeio/zst: update

--- pyeio/zst.py
+++ pyeio/zst.py
@@ -14,7 +14,9 @@ class StreamReader:
         size: int = 1 << 20,
     ) -> None:
         self.path = path
+        self.delimiter = delimiter
         self.size = size
+        self.reset()
 
     def reset(self) -> None:
         self.stream = ZstdDecompressor(
@@ -22,6 +24,32 @@ class StreamReader:
         ).stream_reader(open(self.path, "rb"))
         self.buffer = b""
         self.lines = []
+
+    def __iter__(self) -> Generator[bytes, None, None]:
+        while True:
+            try:
+                yield next(self)
+            except StopIteration:
+                break
+
+    def __next__(self) -> bytes:
+        if len(self.lines):
+            return self.lines.pop(0)
+        else:
+            chunk = self.stream.read(self.size)
+            if chunk:
+                self.lines = (self.buffer + chunk).split(self.delimiter)
+                self.buffer = self.lines[-1]
+                self.lines = self.lines[:-1]
+                return self.lines.pop(0)
+            else:
+                raise StopIteration()
+
+    def read_chunk(self) -> bytes:
+        return self.__next__()
+
+    def read_chunks(self, n: int) -> list[bytes]:
+        return [self.read_chunk() for _ in range(n)]
 
 
 def load(): ...
