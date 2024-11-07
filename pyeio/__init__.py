943d0d6eaa177023ea16fc3d63762665fd151a61
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2024-11-07 08:53:57 -0800

    pyeio/__init__: update imports, comments

--- pyeio/__init__.py
+++ pyeio/__init__.py
@@ -1,7 +1,12 @@
+from pathlib import Path
+from typing import Any
+from pyeio.core import utils
+
+# todo.fix: figure out how to do proper type hints
 # todo: need to add format identification and dynamic module loading based on format to avoid taking a massive amount of time to import
 
 
-def open(): ...
+def open(path: str | Path) -> Any: ...
 
 
 def save(): ...
