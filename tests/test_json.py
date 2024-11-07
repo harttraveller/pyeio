2dc537095613a1c4ed6cd8c95d5fcb8c98b48961
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2024-11-07 08:11:03 -0800

    tests/test_json: update

---
+++ tests/test_json.py
@@ -0,0 +1,7 @@
+from pytest import raises
+from pyeio import json
+from pyeio.base.exceptions import (
+    MissingFileExtensionError,
+    UnsupportedFileExtensionError,
+    IncorrectFileExtensionError,
+)
