79c95fa49a014ec1a492db1cda5db217d9ea7287
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2024-11-08 22:29:32 -0800

    count_lines_in_file: clean up

--- src/count_lines_in_file.rs
+++ src/count_lines_in_file.rs
@@ -2,10 +2,8 @@ use memchr::memchr_iter;
 use memmap2::MmapOptions;
 use rayon::prelude::*;
 use rayon::ThreadPoolBuilder;
-// use std::env;
 use std::fs::File;
 use std::io;
-// use std::time::Instant;
 
 fn count_lines_simd(filename: &str, chunk_size: usize, num_threads: usize) -> io::Result<usize> {
     // Open the file.
@@ -38,37 +36,3 @@ pub fn call(
     let count = count_lines_simd(filename, chunk_size, num_threads)?;
     Ok(count)
 }
-
-// fn main() {
-//     // Get command-line arguments.
-//     let args: Vec<String> = env::args().collect();
-
-//     if args.len() != 4 {
-//         eprintln!(
-//             "Usage: {} <filename> <chunk_size_in_bytes> <num_threads>",
-//             args[0]
-//         );
-//         return;
-//     }
-
-//     let filename = &args[1];
-//     let chunk_size: usize = args[2]
-//         .parse()
-//         .expect("Invalid chunk size (must be a positive integer)");
-//     let num_threads: usize = args[3]
-//         .parse()
-//         .expect("Invalid number of threads (must be a positive integer)");
-
-//     let start_time = Instant::now();
-
-//     match count_lines_simd(filename, chunk_size, num_threads) {
-//         Ok(lines) => {
-//             let duration = start_time.elapsed();
-//             println!("Total number of lines: {}", lines);
-//             println!("Time taken: {:?}", duration);
-//         }
-//         Err(e) => {
-//             eprintln!("Error: {}", e);
-//         }
-//     }
-// }
