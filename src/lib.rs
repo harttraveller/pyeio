fd6cf84d202044b248e52a75bbbf30146eb85034
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2024-11-08 22:31:55 -0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,8 @@ use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
 mod count_lines_in_file;
 
-// this is just a test function
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+fn _test_function(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
 }
 
@@ -18,7 +17,6 @@ fn _count_lines_in_file(
     // Return type changed to PyObject for conversion flexibility
     // Call the main function to count lines
     let count = count_lines_in_file::call(&filename, chunk_size, num_threads)?;
-
     // Convert count directly into a Python integer object
     Ok(count.into_py(py))
 }
@@ -26,7 +24,7 @@ fn _count_lines_in_file(
 #[pymodule]
 #[pyo3(name = "rs")]
 fn pyeio(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(_test_function, m)?)?;
     m.add_function(wrap_pyfunction!(_count_lines_in_file, m)?)?;
     Ok(())
 }
