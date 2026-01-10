a4b22c2bef99bdff031dfe98baf46f9eaf5c4d20
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2026-01-09 20:50:19 -0500

    pyeio: update

--- pyeio/__init__.py
+++ pyeio/__init__.py
@@ -1,2 +1,14 @@
-def hello() -> str:
-    return "Hello from pyeio!"
+# ruff: noqa
+
+from importlib import import_module
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from pyeio import csv
+    from pyeio import json
+    from pyeio import jsonl
+    from pyeio import toml
+    from pyeio import xml
+    from pyeio import yaml
+    from pyeio import zip
+    from pyeio import zst
--- pyeio/io.py
+++ pyeio/io.py
@@ -1,7 +1,39 @@
-def read_string(): ...
-def read_binary(): ...
-def write_string(): ...
-def write_binary(): ...
+# ruff: noqa
+
+from builtins import open
+from io import (
+    DEFAULT_BUFFER_SIZE,
+    SEEK_CUR,
+    SEEK_END,
+    SEEK_SET,
+    BlockingIOError,
+    BufferedIOBase,
+    BufferedRandom,
+    BufferedReader,
+    BufferedRWPair,
+    BufferedWriter,
+    BytesIO,
+    FileIO,
+    IncrementalNewlineDecoder,
+    IOBase,
+    RawIOBase,
+    StringIO,
+    TextIOBase,
+    TextIOWrapper,
+    UnsupportedOperation,
+    open_code,
+    text_encoding,
+)
+from typing import overload, TypeAlias, TypeVar, Literal
+
+StrOrBytes: TypeAlias = str | bytes
+T_StrOrBytes = TypeVar("T_StrOrBytes", bound=StrOrBytes)
+
+
+def read_string() -> str: ...
+def read_binary() -> bytes: ...
+def write_string() -> str: ...
+def write_binary() -> bytes: ...
 
 
 def read(): ...
