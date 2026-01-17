9cf2db27871eadb783724d3b7ec0f679eebe1772
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2026-01-17 13:54:25 -0500

    pyeio/__init__: copy pydantic importer

--- pyeio/__init__.py
+++ pyeio/__init__.py
@@ -1,11 +1,5 @@
-# ruff: noqa
-
-# utils
-from importlib import import_module
-from typing import TYPE_CHECKING
-
-# io
 from builtins import open
+from importlib import import_module
 from io import (
     DEFAULT_BUFFER_SIZE,
     SEEK_CUR,
@@ -29,23 +23,85 @@ from io import (
     open_code,
     text_encoding,
 )
+from typing import TYPE_CHECKING
 
 # io extras
 from pyeio.io import (
-    read_string,
-    read_binary,
-    write_string,
-    write_binary,
-    append_string,
-    append_binary,
-    insert_string,
-    insert_binary,
+    append,
     read,
     write,
-    append,
-    insert,
 )
 
 if TYPE_CHECKING:
-    # file formats
-    from pyeio import json
+    from pyeio import json, toml, yaml
+
+__all__ = [
+    # passthrough
+    "open",
+    "DEFAULT_BUFFER_SIZE",
+    "SEEK_CUR",
+    "SEEK_END",
+    "SEEK_SET",
+    "BlockingIOError",
+    "BufferedIOBase",
+    "BufferedRandom",
+    "BufferedReader",
+    "BufferedRWPair",
+    "BufferedWriter",
+    "BytesIO",
+    "FileIO",
+    "IncrementalNewlineDecoder",
+    "IOBase",
+    "RawIOBase",
+    "StringIO",
+    "TextIOBase",
+    "TextIOWrapper",
+    "UnsupportedOperation",
+    "open_code",
+    "text_encoding",
+    # custom
+    "read",
+    "write",
+    "append",
+    # formats
+    "json",
+    "toml",
+    "yaml",
+]
+
+
+_dynamic_imports: dict[str, tuple[str, str]] = {
+    "json": (__spec__.parent, "__module__"),
+    "toml": (__spec__.parent, "__module__"),
+    "yaml": (__spec__.parent, "__module__"),
+}
+
+
+def __getattr__(attr_name: str) -> object:
+    if attr_name in _deprecated_dynamic_imports:
+        from pydantic.warnings import PydanticDeprecatedSince20
+
+        warn(
+            f"Importing {attr_name} from `pydantic` is deprecated. This feature is either no longer supported, or is not public.",
+            PydanticDeprecatedSince20,
+            stacklevel=2,
+        )
+
+    dynamic_attr = _dynamic_imports.get(attr_name)
+    if dynamic_attr is None:
+        return _getattr_migration(attr_name)
+
+    package, module_name = dynamic_attr
+
+    if module_name == "__module__":
+        result = import_module(f".{attr_name}", package=package)
+        globals()[attr_name] = result
+        return result
+    else:
+        module = import_module(module_name, package=package)
+        result = getattr(module, attr_name)
+        g = globals()
+        for k, (_, v_module_name) in _dynamic_imports.items():
+            if v_module_name == module_name and k not in _deprecated_dynamic_imports:
+                g[k] = getattr(module, k)
+        return result
