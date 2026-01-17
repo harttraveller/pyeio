40c7fd24a7a274d1edf416f97601b39434b54cd1
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2026-01-17 13:55:18 -0500

    pyeio/__init__: update

--- pyeio/__init__.py
+++ pyeio/__init__.py
@@ -77,31 +77,26 @@ _dynamic_imports: dict[str, tuple[str, str]] = {
 }
 
 
-def __getattr__(attr_name: str) -> object:
-    if attr_name in _deprecated_dynamic_imports:
-        from pydantic.warnings import PydanticDeprecatedSince20
-
-        warn(
-            f"Importing {attr_name} from `pydantic` is deprecated. This feature is either no longer supported, or is not public.",
-            PydanticDeprecatedSince20,
-            stacklevel=2,
-        )
-
-    dynamic_attr = _dynamic_imports.get(attr_name)
+def __getattr__(name: str) -> object:
+    dynamic_attr = _dynamic_imports.get(name)
     if dynamic_attr is None:
-        return _getattr_migration(attr_name)
+        raise AttributeError(name)
 
     package, module_name = dynamic_attr
 
     if module_name == "__module__":
-        result = import_module(f".{attr_name}", package=package)
-        globals()[attr_name] = result
+        result = import_module(f".{name}", package=package)
+        globals()[name] = result
         return result
     else:
         module = import_module(module_name, package=package)
-        result = getattr(module, attr_name)
+        result = getattr(module, name)
         g = globals()
         for k, (_, v_module_name) in _dynamic_imports.items():
-            if v_module_name == module_name and k not in _deprecated_dynamic_imports:
+            if v_module_name == module_name:
                 g[k] = getattr(module, k)
         return result
+
+
+def __dir__() -> list[str]:
+    return __all__
